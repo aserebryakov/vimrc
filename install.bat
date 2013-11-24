5aa8a6a07cdf1d7a0e51f0bd715429f1db3dd2b8
Alexander Serebryakov <alex.serebr@gmail.com>
2013-11-24 11:11:19 +0200

    install.bat fixed

--- install.bat
+++ install.bat
@@ -1,4 +1,5 @@
 echo "Installing Vim settings and plugins"
 copy _vimrc "C:\Program Files\Vim\"
 copy vimrc_custom "C:\Program Files\Vim\"
-xcopy vimfiles\* "C:\Program Files\Vim" /f /s /i
+xcopy vimfiles\* "C:\Program Files\Vim\vimfiles" /f /s /i /Y
+pause
